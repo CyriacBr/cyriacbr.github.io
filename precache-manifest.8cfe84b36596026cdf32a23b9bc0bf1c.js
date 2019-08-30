1ae90397df0ed7261a5801dfb220a8e95a9e316c
CyriacBr <cyriacbr@gmail.com>
2019-08-30 19:32:46 +0300

    Deployment

---
+++ precache-manifest.8cfe84b36596026cdf32a23b9bc0bf1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1f567ad5955768ec6e66b0f9a47793",
+    "url": "/index.html"
+  },
+  {
+    "revision": "969d346ffefe477c8444",
+    "url": "/static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "4a432e69d0e61b6e1805",
+    "url": "/static/css/main.4234088a.chunk.css"
+  },
+  {
+    "revision": "969d346ffefe477c8444",
+    "url": "/static/js/2.4d9b5925.chunk.js"
+  },
+  {
+    "revision": "4a432e69d0e61b6e1805",
+    "url": "/static/js/main.af61c67a.chunk.js"
+  },
+  {
+    "revision": "97d543c71138d2232dca",
+    "url": "/static/js/runtime~main.fe177338.js"
+  }
+]);
